e584b783dabc6b9bde433ba73cd7324c58ed139d
goks <pipodecnt@gmail.com>
2012-10-28 03:22:10 -0300

    update test_field.py

--- test_field.py
+++ test_field.py
@@ -1,12 +1,11 @@
 """
 A variety of tests to cover the majority of the functionality
-in mogo Fields.
+in mago Fields.
 """
 
 import unittest
-from mogo import Field, ReferenceField, connect, Model, EnumField
-from mogo.field import EmptyRequiredField
-
+from mago import Field, ReferenceField, connect, Model, EnumField, ConstantField
+from mago.field import FieldError
 
 class Base(object):
     pass
@@ -16,150 +15,163 @@ class Sub(Base):
     pass
 
 
-class MogoFieldTests(unittest.TestCase):
+class MagoFieldTests(unittest.TestCase):
 
     def setUp(self):
-        super(MogoFieldTests, self).setUp()
+        super(MagoFieldTests, self).setUp()
         self._mongo_connection = connect("__test_change_field_name")
 
     def tearDown(self):
-        super(MogoFieldTests, self).tearDown()
+        super(MagoFieldTests, self).tearDown()
         self._mongo_connection.drop_database("__test_change_field_name")
         self._mongo_connection.disconnect()
 
     def test_field(self):
 
-        class MockModel(dict):
-            field = Field(unicode)
+        class MockModel(Model):
+            field = Field(str)
             typeless = Field()
             required = Field(required=True)
-            string = Field(basestring)
-            reference = ReferenceField(Base)
+            default = Field(int, default=4)
+            # reference = ReferenceField(Base)
 
         mock = MockModel()
-        # checks if it is in the model fields (shouldn't be yet)
-        self.assertRaises(AttributeError, getattr, mock, "field")
 
-        # NOW we set up fields.
-        cls_dict = MockModel.__dict__
-        field_names = ["typeless", "required", "field", "string"]
-        MockModel._fields = dict([(cls_dict[v].id, v) for v in field_names])
-        self.assertEqual(mock.field, None)
+        # checks the unassigned values
+        self.assertEqual(NotImplemented, mock.field)
+        self.assertEqual(NotImplemented, mock.other)
+        self.assertEqual(NotImplemented, mock["other"])
+        self.assertEqual(NotImplemented, mock["field"])
 
-        mock.field = u"testing"
-        self.assertEqual(mock.field, "testing")
-        self.assertEqual(mock["field"], "testing")
-        self.assertRaises(TypeError, setattr, mock, "field", 5)
-        mock.required = u"testing"
-        self.assertEqual(mock["required"], "testing")
+        # check setters
+        mock.field = "testing"
+        mock.other = "schemaless"
 
-        # Testing type-agnostic fields
-        mock = MockModel()
-        # shouldn't raise anything
+        # test getters
+        self.assertEqual("schemaless", mock["other"])
+        self.assertEqual("testing", mock["field"])
+        self.assertEqual("schemaless", mock.other)
+        self.assertEqual("testing", mock.field)
+
+        # test default
+        self.assertEqual(mock.default, 4)
+        self.assertEqual(mock["default"], 4)
+
+        # test type constraint
+        self.assertRaises(FieldError, setattr, mock, "field", 5)
+        self.assertRaises(FieldError, setattr, mock, "field", object())
+        self.assertRaises(FieldError, setattr, mock, "default", "object()")
+
+        # test typeless
         mock.typeless = 5
         mock.typeless = "string"
 
-        # Testing issubclass comparison for type checking
-        # neither of these should raise a type error
-        mock = MockModel(string="foobar")
-        mock = MockModel(string=u"foobar")
-
-        base = Base()
-        sub = Sub()
-        mock = MockModel(reference=base)
-        mock = MockModel(reference=sub)
-
-        empty_model = MockModel()
-        # testing that the required field is, you know, required.
-        self.assertRaises(EmptyRequiredField, getattr, empty_model, "required")
-
-    def test_change_field_name(self):
-        """It should allow an override of a field's name."""
-        class MockModel(Model):
-            abbreviated = Field(unicode, field_name="abrv")
-            long_name = Field(unicode, field_name="ln", required=True)
-            regular = Field(unicode, required=True)
-
-        model = MockModel(
-            abbreviated=u"lorem ipsum", long_name=u"malarky", regular=u"meh.")
-
-        # Check the model's dictionary.
-        self.assertTrue("abrv" in model)
-        # Access by friendly name.
-        self.assertEqual(u"lorem ipsum", model.abbreviated)
-        # No access by field_name.
-        self.assertRaises(AttributeError, getattr, model, "abrv")
-
-        # Test save.
-        model.save(safe=True)
-
-        # Test search.
-        fetched = MockModel.search(abbreviated=u"lorem ipsum")
-        self.assertIsNotNone(fetched)
-        fetched = MockModel.search(long_name=u"malarky")
-        self.assertIsNotNone(fetched)
-
-        # Test updates with long names.
-        model.update(abbreviated=u"dolor set")
-        self.assertEqual(u"dolor set", model.abbreviated)
-        fetched = MockModel.search(abbreviated=u"dolor set")
-        self.assertEqual(1, fetched.count())
-
-        model.update(long_name=u"foobar")
-        self.assertEqual(u"foobar", model.long_name)
-        fetched = MockModel.search(long_name=u"foobar")
-        self.assertEqual(1, fetched.count())
-
-        # Test updates with short names.
-        MockModel.update({}, {"$set": {"abrv": u"revia"}})
-        fetched = MockModel.find_one({"abrv": "revia"})
-        self.assertEqual(fetched.abbreviated, "revia")
-
-        # Test finds with short names.
-        fetched = MockModel.find({"ln": "foobar"})
-        self.assertEqual(1, fetched.count())
-        fetched = fetched.first()
-        self.assertEqual(u"revia", fetched.abbreviated)
-
-        # Test search on regular fields.
-        fetched = MockModel.search(regular=u"meh.")
-        self.assertEqual(1, fetched.count())
+        # test required
+        self.assertRaises(FieldError, mock.save)
 
     def test_enum_field(self):
         """ Test the enum field """
-        class EnumModel1(Model):
+        class EnumModel(Model):
             field = EnumField((1, 3, "what"))
 
-        instance = EnumModel1(field=3)
-        self.assertEqual(instance.field, 3)
-        with self.assertRaises(ValueError):
-            instance = EnumModel1(field=False)
+        mock = EnumModel()
+        mock.field = 3
+        self.assertEqual(mock.field, 3)
 
-        class EnumModel2(Model):
-            field = EnumField(lambda x: x.__class__.__name__)
-        EnumModel2(field="EnumModel2")
-        with self.assertRaises(ValueError):
-            EnumModel1(field="nottheclassname")
+        self.assertRaises(FieldError, setattr, mock, "field", 2)
 
-    def test_default_field(self):
-        """ Test that the default behavior works like you'd expect. """
-        class TestDefaultModel(Model):
-            field = Field()  # i.e. no default value
+    def test_const_field(self):
+        class ConstField(Model):
+            field = ConstantField(str)
 
-        entry = TestDefaultModel()
-        self.assertFalse("field" in entry)
+        mock = ConstField()
 
-        self.assertEqual(None, entry.field)
-        self.assertFalse("field" in entry)
+        self.assertEqual(mock.field, NotImplemented)
+        mock.field = "testing"
+        self.assertEqual(mock.field, "testing")
+        mock.field = "keep testing"
+        self.assertEqual(mock.field, "keep testing")
 
-        class TestDefaultModel2(Model):
-            field = Field(default=None)
+        mock.save()
+        self.assertEqual(mock.field, "keep testing")
+        self.assertRaises(FieldError, setattr, mock, "field", "I'm jumping!")
 
-        entry2 = TestDefaultModel2()
-        self.assertTrue("field" in entry2)
-        self.assertEqual(None, entry2.field)
-        self.assertEqual(None, entry2["field"])
+    # def test_change_field_name(self):
+    #     """It should allow an override of a field's name."""
+    #     class MockModel(Model):
+    #         abbreviated = Field(str, field_name="abrv")
+    #         long_name = Field(str, field_name="ln", required=True)
+    #         regular = Field(str, required=True)
 
-        entry3 = TestDefaultModel2(field="foobar")
-        self.assertEqual("foobar", entry3.field)
-        self.assertEqual("foobar", entry3["field"])
+    #     model = MockModel(
+    #         abbreviated=u"lorem ipsum", long_name=u"malarky", regular=u"meh.")
+
+    #     # Check the model's dictionary.
+    #     self.assertTrue("abrv" in model)
+    #     # Access by friendly name.
+    #     self.assertEqual(u"lorem ipsum", model.abbreviated)
+    #     # No access by field_name.
+    #     self.assertRaises(AttributeError, getattr, model, "abrv")
+
+    #     # Test save.
+    #     model.save(safe=True)
+
+    #     # Test search.
+    #     fetched = MockModel.search(abbreviated=u"lorem ipsum")
+    #     self.assertIsNotNone(fetched)
+    #     fetched = MockModel.search(long_name=u"malarky")
+    #     self.assertIsNotNone(fetched)
+
+    #     # Test updates with long names.
+    #     model.update(abbreviated=u"dolor set")
+    #     self.assertEqual(u"dolor set", model.abbreviated)
+    #     fetched = MockModel.search(abbreviated=u"dolor set")
+    #     self.assertEqual(1, fetched.count())
+
+    #     model.update(long_name=u"foobar")
+    #     self.assertEqual(u"foobar", model.long_name)
+    #     fetched = MockModel.search(long_name=u"foobar")
+    #     self.assertEqual(1, fetched.count())
+
+    #     # Test updates with short names.
+    #     MockModel.update({}, {"$set": {"abrv": u"revia"}})
+    #     fetched = MockModel.find_one({"abrv": "revia"})
+    #     self.assertEqual(fetched.abbreviated, "revia")
+
+    #     # Test finds with short names.
+    #     fetched = MockModel.find({"ln": "foobar"})
+    #     self.assertEqual(1, fetched.count())
+    #     fetched = fetched.first()
+    #     self.assertEqual(u"revia", fetched.abbreviated)
+
+    #     # Test search on regular fields.
+    #     fetched = MockModel.search(regular=u"meh.")
+    #     self.assertEqual(1, fetched.count())
+
+
+
+    # def test_default_field(self):
+    #     """ Test that the default behavior works like you'd expect. """
+    #     class TestDefaultModel(Model):
+    #         field = Field()  # i.e. no default value
+
+    #     entry = TestDefaultModel()
+    #     self.assertFalse("field" in entry)
+
+    #     self.assertEqual(None, entry.field)
+    #     self.assertFalse("field" in entry)
+
+    #     class TestDefaultModel2(Model):
+    #         field = Field(default=None)
+
+    #     entry2 = TestDefaultModel2()
+    #     self.assertTrue("field" in entry2)
+    #     self.assertEqual(None, entry2.field)
+    #     self.assertEqual(None, entry2["field"])
+
+    #     entry3 = TestDefaultModel2(field="foobar")
+    #     self.assertEqual("foobar", entry3.field)
+    #     self.assertEqual("foobar", entry3["field"])
+
+if __name__ == "__main__":
+    unittest.main()
