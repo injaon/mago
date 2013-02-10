ca80b7ae53b4ae0dcff742e81793fbf66bbb9ca8
injaon <injaon@gmail.com>
2013-02-10 04:26:15 -0300

    add decorator for tracking model's changes

--- mago/decorators.py
+++ mago/decorators.py
@@ -1,14 +1,9 @@
-"""
-Just various decorators.
-"""
+import mago
+"""Just various decorators."""
 
 
 class notinstancemethod(object):
-    """
-    Used to refuse access to a classmethod if called from
-    an instance.
-    """
-
+    """Used to refuse access to a classmethod if called from an instance."""
     def __init__(self, func):
         self.func = classmethod(func)
 
@@ -16,3 +11,24 @@ class notinstancemethod(object):
         if obj is not None:
             raise TypeError("Cannot call this method on an instance.")
         return self.func.__get__(obj, objtype)
+
+def register_dirty(func):
+    def _register(self, name, *args, **kwargs):
+        old = self.get(name, mago.UnSet)
+        res = func(self, name, *args, **kwargs)
+
+        if old != self.get(name, mago.UnSet) and self.session \
+          and self.session.is_active:
+            self.session._register_change(self, name, old)
+        return res
+    return _register
+
+
+
+# def singleton(cls):
+#     instances = {}
+#     def getinstance():
+#         if cls not in instances:
+#             instances[cls] = cls()
+#         return instances[cls]
+#     return getinstance
